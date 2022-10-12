cfb3855720beef4f4e746acde02236aa30c06f33
0SCAR0421 <oscar0421@kakao.com>
2022-10-12 15:27:33 +0900

    Feat: 페이지 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>Hello World!</div>;
-}
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Sign from './pages/Sign/Sign';
+import Error from './pages/Error';
+import TodoList from './pages/TodoList';
+
+const App = () => {
+  useEffect(() => {
+    localStorage.getItem('key');
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="*" element={<Error />}></Route>
+      <Route path="/" element={<Sign />}></Route>
+      <Route path="/todo" element={<TodoList />}></Route>
+    </Routes>
+  );
+};
 
 export default App;
