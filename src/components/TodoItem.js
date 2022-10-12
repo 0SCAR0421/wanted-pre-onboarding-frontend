80c9061585b1b6674620a3ec06eca348926c82a2
0SCAR0421 <oscar0421@kakao.com>
2022-10-12 19:37:31 +0900

    Style: TodoItem 컴포넌트 스타일링

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,35 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+import { TodoButton } from '../components/CommonTodo';
+import { CommonInput } from './Common';
+
+const TodoItemContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: var(--space2);
+  color: var(--text3);
+`;
+
+const TodoItemContentBox = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    padding: calc(var(--space3) / 2);
+    width: 300px;
+    height: 40px;
+    word-break: break-all;
+  }
+`;
+
+const TodoItemInput = styled(CommonInput)`
+  margin-top: 0;
+  width: 300px;
+  padding: calc(var(--space3) / 2);
+  font-size: var(--font-size1);
+`;
+
+const TodoItemButtonBox = styled.div``;
 
 const TodoItem = ({
   id,
@@ -24,35 +55,47 @@ const TodoItem = ({
   };
 
   return (
-    <div>
-      <span> {isCompleted ? 'true' : 'false'}</span>
+    <TodoItemContainer>
       {mode ? (
         <>
-          <input onChange={handleModifyData} value={todoData.todo}></input>
-          <button
-            onClick={() => {
-              handleModifyTodo(id, todoData);
-              handleChangeMode();
-            }}
-          >
-            확인
-          </button>
-          <button onClick={handleChangeMode}>취소</button>
+          <TodoItemContentBox>
+            <TodoItemInput
+              onChange={handleModifyData}
+              value={todoData.todo}
+            ></TodoItemInput>
+          </TodoItemContentBox>
+          <TodoItemButtonBox>
+            <span> {isCompleted ? 'true' : 'false'}</span>
+            <TodoButton
+              onClick={() => {
+                handleModifyTodo(id, todoData);
+                handleChangeMode();
+              }}
+            >
+              확인
+            </TodoButton>
+            <TodoButton onClick={handleChangeMode}>취소</TodoButton>
+          </TodoItemButtonBox>
         </>
       ) : (
         <>
-          <span>{todo} </span>
-          <button onClick={handleChangeMode}>수정</button>
+          <TodoItemContentBox>
+            <p>{todo} </p>
+          </TodoItemContentBox>
+          <TodoItemButtonBox>
+            <span> {isCompleted ? 'true' : 'false'}</span>
+            <TodoButton onClick={handleChangeMode}>수정</TodoButton>
+            <TodoButton
+              onClick={() => {
+                handleDelete(id);
+              }}
+            >
+              삭제
+            </TodoButton>
+          </TodoItemButtonBox>
         </>
       )}
-      <button
-        onClick={() => {
-          handleDelete(id);
-        }}
-      >
-        삭제
-      </button>
-    </div>
+    </TodoItemContainer>
   );
 };
 
