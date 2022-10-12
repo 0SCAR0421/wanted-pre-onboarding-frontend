1d0730d4febda8e2019d62a506036f81155dd388
0SCAR0421 <oscar0421@kakao.com>
2022-10-12 19:35:02 +0900

    Chore: headers 변경

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 const url = process.env.REACT_APP_API_URL;
-axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(
-  'access_token'
-)}`;
 
 export const postSignup = async (data) => {
   const res = await axios.post(`${url}/auth/signup`, {
@@ -23,30 +20,54 @@ export const postSignin = async (data) => {
 };
 
 export const postTodo = async (data) => {
-  const res = await axios.post(`${url}/todos`, {
-    todo: data,
-  });
+  const res = await axios.post(
+    `${url}/todos`,
+    {
+      todo: data,
+    },
+    {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+    }
+  );
 
   return res.data;
 };
 
 export const getTodoList = async () => {
-  const res = await axios.get(`${url}/todos`);
-
-  return res.data;
-};
-
-export const putTodo = async (id, data) => {
-  const res = await axios.put(`${url}/todos/${id}`, {
-    todo: data.todo,
-    isCompleted: data.isCompleted,
+  const res = await axios.get(`${url}/todos`, {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('access_token')}`,
+    },
   });
 
   return res.data;
 };
 
+export const putTodo = async (id, data) => {
+  const res = await axios.put(
+    `${url}/todos/${id}`,
+    {
+      todo: data.todo,
+      isCompleted: data.isCompleted,
+    },
+    {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      },
+    }
+  );
+
+  return res.data;
+};
+
 export const deleteTodo = async (id) => {
-  const res = await axios.delete(`${url}/todos/${id}`);
+  const res = await axios.delete(`${url}/todos/${id}`, {
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('access_token')}`,
+    },
+  });
 
   return res.data;
 };
